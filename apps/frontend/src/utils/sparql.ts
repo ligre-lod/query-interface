404b8c11cb035dc5548eac6bfacff0247964fee7
PaulienLem <paulien.lemay@gmail.com>
2026-09-04 14:27:03 +0200

    Use temporary url for testing purposes

--- apps/frontend/src/utils/sparql.ts
+++ apps/frontend/src/utils/sparql.ts
@@ -201,7 +201,7 @@ export function generateSparqlQuery(filters: SearchFilters): string {
   return `
 SELECT ?subject ?wrs ?pos ?lexicons where {
   {SELECT ?subject ?poslink ?pos (group_concat(distinct ?wr ; separator=" ") as ?wrs) (group_concat(distinct ?lexicon ; separator=" ") as ?lexicons) WHERE {
-      ?subject <http://purl.org/dc/terms/isPartOf> <http://ligre-erc.eu/data/id/lemma/LemmaBank> .
+      ?subject <http://purl.org/dc/terms/isPartOf> <https://dev.ligre.ugent.be/data/id/lemma/LemmaBank> .
       ${conditionsString}
       ?subject <http://lila-erc.eu/ontologies/lila/hasPOS> ?poslink .
       BIND(?poslink AS ?pos) .
