64b1d8c497c9dd9800ee70c002ab7d9bc36246c5
PaulienLem <paulien.lemay@gmail.com>
2026-09-08 13:08:53 +0200

    Add homepage

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,18 +1,11 @@
-import {
-  AppBar,
-  Box,
-  Container,
-  CssBaseline,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import type React from 'react';
 import { useState } from 'react';
-import FloatingLogo from './components/FloatingLogo';
-import NavigationLinks from './components/NavigationLinks';
-import Search from './components/Search';
-import ThemeSwitcher from './components/ThemeSwitcher';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
 import { darkTheme, lightTheme } from './themes';
 
 const App: React.FC = () => {
@@ -27,29 +20,21 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Box
-        sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
-      >
-        <AppBar position="static" elevation={0}>
-          <Toolbar sx={{ justifyContent: 'center', py: 2 }}>
-            {/*<FloatingLogo />*/}
-            <Box sx={{ textAlign: 'center' }}>
-              <Typography variant="h1" component="h1" sx={{ mb: 0.5 }}>
-                LiGre Query Interface
-              </Typography>
-              <NavigationLinks />
-            </Box>
-            <ThemeSwitcher
-              mode={isDarkMode ? 'dark' : 'light'}
-              onToggleTheme={toggleTheme}
-            />
-          </Toolbar>
-        </AppBar>
-
-        <Container component="main" sx={{ flex: 1, py: 4, maxWidth: 'lg' }}>
-          <Search />
-        </Container>
-      </Box>
+      <HashRouter>
+        <Routes>
+          <Route
+            element={
+              <Layout
+                mode={isDarkMode ? 'dark' : 'light'}
+                onToggleTheme={toggleTheme}
+              />
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="search" element={<SearchPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   );
 };
